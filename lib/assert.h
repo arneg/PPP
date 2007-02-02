3f1cefb27b7433e80a7b8c523709f0ff4bdc84bd
tobij <tobij>
2007-02-02 01:01:11 +0000

    super cool assertions

---
+++ lib/assert.h
@@ -0,0 +1,11 @@
+#ifndef _ASSERT_H
+# define _ASSERT_H
+# if (defined(DEBUG) && !defined(NO_ASSERT)) || defined(ASSERT)
+#  define assert(x)	if (!(x)) error("Assertion (" #x ") failed.\n")
+# else
+#  define assert(x)
+# endif
+
+# define enforce(x)	if(!(x)) error("Enforcing (" #x ") with this " \
+				       "throw.")
+#endif
