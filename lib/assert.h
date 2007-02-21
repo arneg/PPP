2c8f23a24115837ea3baf74ace4bf53815aad878
p0rtage <p0rtage>
2007-02-21 22:17:16 +0000

    r for reason

--- lib/assert.h
+++ lib/assert.h
@@ -3,12 +3,15 @@
 
 # if (defined(DEBUG) && !defined(NO_ASSERT)) || defined(ASSERT)
 #  define assert(x)	enforce(x)
-#  define assert(x,r)	enforce(x,r)
+// r for reason
+#  define assertr(x,r)	enforcer(x,r)
 # else
 #  define assert(x)	(0)
-#  define assert(x,r)	(0)
+// r for reason
+#  define assertr(x,r)	(0)
 # endif
 
 # define enforce(x)	(!(x) && error("Assertion (" #x ") failed.\n"))
-# define enforce(x,r)	(!(x) && error(r + "\n"))
+// r for reason
+# define enforcer(x,r)	(!(x) && error(r + "\n"))
 #endif
