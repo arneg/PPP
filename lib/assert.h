92af6b43f4dbf808013e5516b7df95ef43a240cb
p0rtage <p0rtage>
2007-02-21 21:54:39 +0000

    assertions with messages

--- lib/assert.h
+++ lib/assert.h
@@ -3,9 +3,12 @@
 
 # if (defined(DEBUG) && !defined(NO_ASSERT)) || defined(ASSERT)
 #  define assert(x)	enforce(x)
+#  define assert(x,r)	enforce(x,r)
 # else
 #  define assert(x)	(0)
+#  define assert(x,r)	(0)
 # endif
 
 # define enforce(x)	(!(x) && error("Assertion (" #x ") failed.\n"))
+# define enforce(x,r)	(!(x) && error(r + "\n"))
 #endif
