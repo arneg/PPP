400151c824c9dc07b79ff3f2459a46d9afc035e1
Arne Goedeke <el@laramies.com>
2009-08-04 09:49:44 +0200

    indent

--- js/utf8.js
+++ js/utf8.js
@@ -55,46 +55,46 @@ UTF8.decode = function(str) {
     var mark = 0;
     
     for (var i = 0; i < str.length; i++) {
-	var c = str.charCodeAt(i);
+		var c = str.charCodeAt(i);
 
-	if (c > 0x7f) {
-	    if (mark != i) ret += str.substring(mark, i);
+		if (c > 0x7f) {
+			if (mark != i) ret += str.substring(mark, i);
 
-	    if (c >= 0xc2 && c <= 0xdf && i+1 < str.length) { // 2 byte
-		var c2 = str.charCodeAt(i+1);
-		ret += String.fromCharCode(((c & 0x1f) << 6) | (c2 & 0x3f));
-		i++;
-	    } else if (c >= 0xe0 && c <= 0xef && i+2 < str.length) { // 3 byte
-		var c2 = str.charCodeAt(i+1);
-		var c3 = str.charCodeAt(i+2);
+			if (c >= 0xc2 && c <= 0xdf && i+1 < str.length) { // 2 byte
+				var c2 = str.charCodeAt(i+1);
+				ret += String.fromCharCode(((c & 0x1f) << 6) | (c2 & 0x3f));
+				i++;
+			} else if (c >= 0xe0 && c <= 0xef && i+2 < str.length) { // 3 byte
+				var c2 = str.charCodeAt(i+1);
+				var c3 = str.charCodeAt(i+2);
 
-		ret += String.fromCharCode(((c & 0x0f) << 12) | ((c2 & 0x3f) << 6) | (c3 & 0x3f));
+				ret += String.fromCharCode(((c & 0x0f) << 12) | ((c2 & 0x3f) << 6) | (c3 & 0x3f));
 
-		i+=2;
-	    } else if (c >= 0xf0 && c <= 0xf4 && i+3 < str.length) { // 4 byte
-		var c2 = str.charCodeAt(i+1);
-		var c3 = str.charCodeAt(i+2);
-		var c4 = str.charCodeAt(i+3);
+				i+=2;
+			} else if (c >= 0xf0 && c <= 0xf4 && i+3 < str.length) { // 4 byte
+				var c2 = str.charCodeAt(i+1);
+				var c3 = str.charCodeAt(i+2);
+				var c4 = str.charCodeAt(i+3);
 
-		ret += String.fromCharCode(  ((c  & 0x07) << 18) 
-					   | ((c2 & 0x3f) << 12) 
-					   | ((c3 & 0x3f) << 6) 
-					   | (c4 & 0x3f));
-		i+=3;
-	    } else {
-		throw("Invalid UTF8 sequence at pos "+i);
-	    }
+				ret += String.fromCharCode(  ((c  & 0x07) << 18) 
+							   | ((c2 & 0x3f) << 12) 
+							   | ((c3 & 0x3f) << 6) 
+							   | (c4 & 0x3f));
+				i+=3;
+			} else {
+				throw("Invalid UTF8 sequence at pos "+i);
+			}
 
-	    mark = i+1;
-	}
+			mark = i+1;
+		}
     }
 
     if (mark != str.length) {
-	if (mark == 0) {
-	    return str;
-	} else {
-	    ret += str.substring(mark, str.length); 
-	}
+		if (mark == 0) {
+			return str;
+		} else {
+			ret += str.substring(mark, str.length); 
+		}
     }
 
     return ret;
