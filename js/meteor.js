9d2cd02de24fa3984a9a7488ed7a77175ce70063
Arne Goedeke <el@laramies.com>
2009-08-26 15:48:22 +0200

    xhttpr

--- js/meteor.js
+++ js/meteor.js
@@ -1,3 +1,10 @@
+if( typeof XMLHttpRequest == "undefined" ) XMLHttpRequest = function() {
+  try { return new ActiveXObject("Msxml2.XMLHTTP.6.0") } catch(e) {}
+  try { return new ActiveXObject("Msxml2.XMLHTTP.3.0") } catch(e) {}
+  try { return new ActiveXObject("Msxml2.XMLHTTP") } catch(e) {}
+  try { return new ActiveXObject("Microsoft.XMLHTTP") } catch(e) {}
+  throw new Error( "This browser does not support XMLHttpRequest." )
+};
 /**
  * @namespace Meteor js connection namespace.
  */
