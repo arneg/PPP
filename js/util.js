a8bce3430e66d2315e45044de10c276c1e2f6d1f
Arne Goedeke <el@laramies.com>
2009-10-01 13:18:32 +0200

    .

--- js/util.js
+++ js/util.js
@@ -56,3 +56,47 @@ UTIL.split_replace = function(reg, s, cb) {
 
 	return ret;
 }
+UTIL.has_prefix = function(s, n) {
+	if (s.length < n.length) return false;
+	return (n == s.substr(0, n.length));
+}
+UTIL.has_suffix = function(s, n) {
+	if (s.length < n.length) return false;
+	return (n == s.substr(s.length-n.length, n.length));
+}
+UTIL.search_array = function(a, n) {
+	for (var i = 0; i < a.length; i++) {
+		if (n == a[i]) return i;
+	}
+
+	return -1;
+}
+UTIL.replaceClass = function(o, cl1, cl2) {
+	var classes = o.className.split(' ');
+	var i = UTIL.search_array(classes, cl1);
+	var j = UTIL.search_array(classes, cl2);
+
+	if (i == -1 && j == -1) {
+		classes.push(cl2);
+	} else if (i == -1) {
+		return;
+	} else if (j == -1) {
+		classes[i] = cl2;
+	} else {
+		classes.splice(i, 1);
+	}
+	o.className = classes.join(" ");
+}
+UTIL.addClass = function(o, cl) {
+	var classes = o.className.split(' ');
+	var i = UTIL.search_array(classes, cl);
+
+	if (i == -1) {
+		classes.push(cl);
+		o.className = classes.join(" ");
+	}
+}
+UTIL.hasClass = function(o, cl) {
+	var classes = o.className.split(' ');
+	return (-1 != UTIL.search_array(classes, cl));
+}
