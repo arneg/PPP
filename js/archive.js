359f54c47d91391cdeb22516912019fee67e1db4
Arne Goedeke <el@laramies.com>
2010-04-16 02:50:45 +0200

    paged search

--- js/archive.js
+++ js/archive.js
@@ -29,6 +29,7 @@ ARCHIVE.SearchRequest = Base.extend({
 		document.getElementsByTagName("head")[0].removeChild(this.script);
 		delete ARCHIVE.Requests[this.id];
 		this.callback(result);
+		if (this.hasOwnProperty("timeout_id")) window.clearTimeout(this.timeout_id);
 	},
 	timeout : function() {
 		this.response(ARCHIVE.TIMEOUT);
@@ -41,7 +42,7 @@ ARCHIVE.Search = Base.extend({
 		    q : what,
 		    rows : (!!params && !!params.per_page) ? params.per_page : 10
 		};
-		this.vars["fl[]"] = [ "avg_rating", "description", "title", "mediatype", "downloads", "language", "contributor", "identifier" ];
+		this.vars["fl[]"] = [ "avg_rating", "description", "title", "mediatype", "downloads", "language", "contributor", "identifier", "creator" ];
 		//this.vars["fl[]"] = "title,identifier";
 	},
 	search : function(page, callback) {
@@ -59,12 +60,13 @@ ARCHIVE.Search = Base.extend({
 		var r = new ARCHIVE.SearchRequest(id, callback, script);
 		ARCHIVE.Requests[id] = r;
 		document.getElementsByTagName("head")[0].appendChild(script);
-		window.setTimeout(UTIL.make_method(r, r.timeout), 100000);
+		r.timeout_id = window.setTimeout(UTIL.make_method(r, r.timeout), 10000);
 	}
 });
 ARCHIVE.Item = Base.extend({
-	constructor : function(id) {
-		this.identifier = id;
+	constructor : function(info) {
+		this.identifier = info.identifier;
+		this.info = info;
 		this.callbacks = [];
 	},
 	get_url : function() {
@@ -106,5 +108,55 @@ ARCHIVE.Item = Base.extend({
 	timeout : function() {
 		this.result(ARCHIVE.TIMEOUT);
 		delete this.info;
+	}
+});
+ARCHIVE.PagedSearch = ARCHIVE.Search.extend({
+	constructor : function(node, what, per_page) {
+		this.per_page = per_page;
+		this.base(what, { per_page : per_page });
+		this.next = new WIDGET.SimpleButton(document.createElement("div"), { hover : "hover" }, { click : UTIL.make_method(this, this.next) });
+		UTIL.addClass(this.next.node, "next");
+		this.prev = new WIDGET.SimpleButton(document.createElement("div"), { hover : "hover" }, { click : UTIL.make_method(this, this.prev) });
+		UTIL.addClass(this.prev.node, "prev");
+		this.container = document.createElement("div");
+		UTIL.addClass(this.container, "container");
+		this.info = document.createElement("div");
+		UTIL.addClass(this.info, "info");
+		node.appendChild(this.prev.node);
+		node.appendChild(this.container);
+		node.appendChild(this.next.node);
+		node.appendChild(this.info);
+		this.page = 1;
+		this.pages = {};
+		this.search(1);
 	},
+	next : function() { if (this.page + 1 <= this.num_pages) this.search(this.page + 1); },
+	prev : function() { if (this.page > 1) this.search(this.page - 1); },
+	search : function(page) {
+		if (this.pages.hasOwnProperty(page)) this.display_page(page, this.pages[page]);
+		else this.base(page, UTIL.make_method(this, function (r) { this.pages[page] = r; this.display_page(page, r) }));
+	},
+	display_page : function(page, r) {
+		var list = r.response.docs;
+		var i;
+		this.results = r.response.numFound;
+		this.num_pages = Math.ceil(this.results/this.per_page);
+		this.page = page;
+		this.display_info(r);
+		this.container.innerHTML = "";
+
+		for (i = 0; i < list.length; i++) {
+		    this.container.appendChild(this.display_item(i, new ARCHIVE.Item(list[i])))
+		}
+	},
+	display_item : function(n, item) {
+		var div = document.createElement("div");
+		UTIL.addClass(div, "item");
+		div.innerHTML = UTIL.render_html("<h1 class=identifier>[identifier]</h1><p class=description>[description]</p>", item.info);
+		return div;
+	},
+	display_info : function(r) {
+		this.info.innerHTML = "";
+		UTIL.render_into(this.info, "[span|page] of [span|num_pages]", this);
+	}
 });
