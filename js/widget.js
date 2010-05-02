c8e70bb0fe9b24039d627a2efbc746624b394e43
Arne Goedeke <el@laramies.com>
2010-05-02 02:50:31 +0200

    some bugfixes and working cycle widget

--- js/widget.js
+++ js/widget.js
@@ -1,3 +1,4 @@
+WIDGET = {};
 WIDGET.StateMachine = Base.extend({
 	constructor : function(transitions, actions) {
 		this.t = transitions;
@@ -27,13 +28,27 @@ WIDGET.StateMachine = Base.extend({
 	registerEvent : function(e, fun) {
 		if (this.a.hasOwnProperty(e)) this.a[e].push(fun);
 		else this.a[e] = [ fun ];
+	},
+	hasEvent : function(e) {
+		return this.a.hasOwnProperty(e);
 	}
 });
 WIDGET.Base = WIDGET.StateMachine.extend({
 	constructor : function(node, states, actions) {
+		this.node = node;
 		var transitions = {
 			start : {}
 		};
+
+		if (UTIL.arrayp(states)) {
+			var m = {};
+			for (var i = 0; i < states.length; i++) {
+				m[states[i]] = 1;
+			}
+			states = m;
+		}
+
+		if (!actions) actions = {};
 
 		if (states.hover) {
 			transitions.start.mouseover = "hover";
@@ -43,10 +58,22 @@ WIDGET.Base = WIDGET.StateMachine.extend({
 		}
 
 		if (states.clicked) {
-			transitions.start.mousedown = "clicked";
+			if (states.hover) {
+			    transitions.hover.mousedown = "clicked";
+			} else {
+			    transitions.start.mousedown = "clicked";
+			}
+			// we go back to start because someone could click and then move out of
+			// the widget
 			transitions.clicked = { mouseup : "start" };
+
 			node.onmousedown = UTIL.make_method(this, this.trigger, "mousedown");
 			node.onmouseup = UTIL.make_method(this, this.trigger, "mouseup");
+		}
+
+		if (states.blur) {
+			transitions.start.blur = "blur";
+			transitions.blur = { focus : "start" };
 		}
 
 		var l = [ "mouseover", "mouseout", "mousedown", "mouseup", "click", "dblclick" ];
@@ -57,7 +84,14 @@ WIDGET.Base = WIDGET.StateMachine.extend({
 		}
 
 		this.base(transitions, actions);
+
+		if (!actions.hover) this.registerEvent("hover", UTIL.make_method(window, UTIL.replaceClass, this.node, "start", "hover"));
+		if (!actions.clicked) this.registerEvent("clicked", UTIL.make_method(window, UTIL.replaceClass, this.node, "start", "clicked"));
 	},
+	registerEvent : function(e, fun) {
+		if (!this.hasEvent(e)) this.node["on"+e] = UTIL.make_method(this, this.trigger, e);
+		this.base(e, fun);
+	}
 });
 WIDGET.SimpleButton = WIDGET.Base.extend({
 	constructor : function(node, classes, actions) {
@@ -67,6 +101,31 @@ WIDGET.SimpleButton = WIDGET.Base.extend({
 		}
 
 		this.base(node, classes, actions);
-	},
+	}
 });
+WIDGET.Cycle = Base.extend({
+	constructor : function() {
+		this.items = [];
+	},
+	cycle : function(n) {
+		if (this.items.length == 1) return;
+		this.items[0].trigger("blur");
+		if (n > 0) while (n-- > 0) this.items.push(this.items.shift());
+		else if (n < 0) while (n++ > 0) this.items.unshift(this.items.pop());
+		this.items[0].trigger("focus");
+	},
+	addItem : function(widget) {
+		this.items.push(widget);
+		widget.registerEvent(">blur", UTIL.make_method(window, UTIL.addClass, widget.node, "blur"));
+		widget.registerEvent("<blur", UTIL.make_method(window, UTIL.removeClass, widget.node, "blur"));
 
+		if (this.items.length == 1) {
+			widget.trigger("focus");
+		} else {
+			widget.trigger("blur");
+		}
+	},
+	addButton : function(button, step) {
+		button.registerEvent("click", UTIL.make_method(this, this.cycle, step||1));
+	}
+});
