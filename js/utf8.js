0a6af1ba7c2cd19b4c116c954d0808b47fa9b8e3
Arne Goedeke <el@laramies.com>
2011-08-17 03:59:45 +0200

    Optimization: use UTIL.StringBuilder

--- js/utf8.js
+++ js/utf8.js
@@ -25,7 +25,7 @@ UTF8 = new Object();
  * @returns The UTF8 encoded string.
  */
 UTF8.encode = function(str) {
-    var ret = "";
+    var buf = new UTIL.StringBuilder();
     var mark = 0;
     
     for (var i = 0; i < str.length; i++) {
@@ -34,15 +34,20 @@ UTF8.encode = function(str) {
 	if (c > 0x7f) {
 	    if (mark != i) {
 		var t = str.substring(mark, i);
-		ret += t;
+		buf.add(t);
 	    }
 	    
 	    if (c >= 0x80 && c <= 0x07ff) { // 2 byte
-		ret += String.fromCharCode(0xc0 | (c>>6), 0x80 | (c & 0x3f)); 
+		buf.putchar(0xc0 | (c>>6), 0x80 | (c & 0x3f));
 	    } else if ((c >= 0x0800 && c <= 0xd7ff) || (c >= 0xe000 && c <= 0xffff)) { // 3 byte split by non u-chars
-		ret += String.fromCharCode(0xe0 | (c>>12), 0x80 | (0x3f & (c>>6)), 0x80 | (c & 0x3f)); 
+		buf.putchar(0xe0 | (c>>12),
+			    0x80 | (0x3f & (c>>6)),
+			    0x80 | (c & 0x3f));
 	    } else if (c >= 0x10000 && c <= 0x10ffff) { // 4 byte
-		ret += String.fromCharCode(0xf0 | (c>>18), 0x80 | (0x3f & (c>>12)), x80 | (0x3f & (c>>6)), 0x80 | (c & 0x3f)); 
+		buf.putchar(0xf0 | (c>>18),
+			    0x80 | (0x3f & (c>>12)),
+			    0x80 | (0x3f & (c>>6)),
+			    0x80 | (c & 0x3f));
 	    } else {
 		throw("trying to encode non-unicode char " + c);
 	    }
@@ -55,11 +60,11 @@ UTF8.encode = function(str) {
 	if (mark == 0) {
 	    return str;
 	} else {
-	    ret += str.substring(mark, str.length); 
+	    buf.add(str.substring(mark, str.length));
 	}
     }
 
-    return ret;
+    return buf.get();
 };
 /**
  * @param {String} str UTF8 encoded Unicode string.
@@ -67,24 +72,24 @@ UTF8.encode = function(str) {
  * @returns The decoded Unicode string.
  */
 UTF8.decode = function(str) {
-    var ret = "";
+    var buf = new UTIL.StringBuilder();
     var mark = 0;
     
     for (var i = 0; i < str.length; i++) {
 		var c = str.charCodeAt(i) & 0xff;
 
 		if (c > 0x7f) {
-			if (mark != i) ret += str.substring(mark, i);
+			if (mark != i) buf.add(str.substring(mark, i));
 
 			if (c >= 0xc2 && c <= 0xdf && i+1 < str.length) { // 2 byte
 				var c2 = str.charCodeAt(i+1) & 0xff;
-				ret += String.fromCharCode(((c & 0x1f) << 6) | (c2 & 0x3f));
+				buf.putchar(((c & 0x1f) << 6) | (c2 & 0x3f));
 				i++;
 			} else if (c >= 0xe0 && c <= 0xef && i+2 < str.length) { // 3 byte
 				var c2 = str.charCodeAt(i+1) & 0xff;
 				var c3 = str.charCodeAt(i+2) & 0xff;
 
-				ret += String.fromCharCode(((c & 0x0f) << 12) | ((c2 & 0x3f) << 6) | (c3 & 0x3f));
+				buf.putchar(((c & 0x0f) << 12) | ((c2 & 0x3f) << 6) | (c3 & 0x3f));
 
 				i+=2;
 			} else if (c >= 0xf0 && c <= 0xf4 && i+3 < str.length) { // 4 byte
@@ -92,7 +97,7 @@ UTF8.decode = function(str) {
 				var c3 = str.charCodeAt(i+2) & 0xff;
 				var c4 = str.charCodeAt(i+3) & 0xff;
 
-				ret += String.fromCharCode(  ((c  & 0x07) << 18) 
+				buf.putchar(((c  & 0x07) << 18)
 							   | ((c2 & 0x3f) << 12) 
 							   | ((c3 & 0x3f) << 6) 
 							   | (c4 & 0x3f));
@@ -109,9 +114,9 @@ UTF8.decode = function(str) {
 		if (mark == 0) {
 			return str;
 		} else {
-			ret += str.substring(mark, str.length); 
+			buf.add(str.substring(mark, str.length));
 		}
     }
 
-    return ret;
+    return buf.get();
 };
